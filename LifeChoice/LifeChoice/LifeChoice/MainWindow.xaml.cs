505ccdcd03887fd48b9754d05993c1e27717219f
BloodMarch <balidas.benjamin@gmail.com>
2017-09-24 22:51:21 +0200

    initial commit

---
+++ LifeChoice/LifeChoice/LifeChoice/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using LifeChoice.Vues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LifeChoice
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindowLoaded;
+        }
+
+        void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindowLoaded;
+            MenuJeu menu = new MenuJeu();
+            menu.Dispatcher.BeginInvoke((SendOrPostCallback)delegate
+            {
+                Thread.Sleep(3000);
+                this.Close();
+                menu.Show();
+            }, new object[] { null });
+        }
+    }
+}
